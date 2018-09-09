69b7f4a455d428d796ecf84ec28201b94e7b68e0
Desan <lizardesan@gmail.com>
2018-09-09 16:10:32 +0300

    phaser typing and initial Game class

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World")
+/// <reference path="../typings/phaser.d.ts" />
+import { Game as PhaserGame } from "phaser"
+import { config } from "./config"
+
+class Game extends PhaserGame {
+}
+
+export default new Game(config)
